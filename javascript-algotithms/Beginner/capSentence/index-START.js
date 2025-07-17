5d4365a2079b5b4ac0e7722615750c6ba7ea2647
Jesulayomy <jesulayomi081@gmail.com>
2025-07-17 13:00:00 -0400

    [Tardis]: capSentence start refactor

--- javascript-algotithms/Beginner/capSentence/index-START.js
+++ javascript-algotithms/Beginner/capSentence/index-START.js
@@ -1,20 +1,16 @@
-/* CHALLENGE
-Given a sentence containing two or more words, 
-return the equivalent of the sentence when capitalised. E.g
-  capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
-*/
-
-
-
+/** CHALLENGE
+ * Given a sentence containing two or more words,
+ * return the equivalent of the sentence when capitalised. E.g
+ * capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!'
+ */
 
 function capSentence(text) {
-  let wordsArray = text.toLowerCase().split(' ')
-  let capsArray = []
-  wordsArray.forEach(word => {
+  let wordsArray = text.toLowerCase().split(" ");
+  let capsArray = [];
+  wordsArray.forEach((word) => {
     capsArray.push(word[0].toUpperCase() + word.slice(1));
   });
-  return capsArray.join(' ');
-};
+  return capsArray.join(" ");
+}
 
-
-module.exports = capSentence
+module.exports = capSentence;
