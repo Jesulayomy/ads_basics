311faee08fdef0d689f1fd2fea4891bb978ffe0b
Jesulayomy <jesulayomi081@gmail.com>
2025-07-20 13:00:00 -0400

    [Tardis]: Group Anagrams

--- neetcode/arrays&hashing/group_anagrams.js
+++ neetcode/arrays&hashing/group_anagrams.js
@@ -1,11 +1,11 @@
 function groupAnagrams(strs) {
   let groups = {};
-  for(let i = 0; i < strs.length; i++) {
+  for (let i = 0; i < strs.length; i++) {
     let count = new Array(26).fill(0);
-    for(let j = 0; j < strs[i].length; j++) {
-      count[strs[i].charCodeAt(j) - 'a'.charCodeAt(0)] += 1;
+    for (let j = 0; j < strs[i].length; j++) {
+      count[strs[i].charCodeAt(j) - "a".charCodeAt(0)] += 1;
     }
-    groups[count] ? groups[count].push(strs[i]) : groups[count] = [strs[i]];
+    groups[count] ? groups[count].push(strs[i]) : (groups[count] = [strs[i]]);
   }
   return Object.values(groups);
 }
