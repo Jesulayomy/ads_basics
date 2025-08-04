2d893364ae86984aa7e9d5ad174a338033e6d6db
Jesulayomy <jesulayomi081@gmail.com>
2025-08-03 20:02:53 -0400

    [Redacted]: Problem sets

--- problem-sets/calculating_concurrent_customer_calls-main/utils/sortCallsByStartTimestamp.js
+++
@@ -1,5 +0,0 @@
-const sortCallsByStartTimestamp = (calls) => {
-  return calls.sort((a, b) => a.startTimestamp - b.startTimestamp);
-}
-
-module.exports = { sortCallsByStartTimestamp };
--- problem-sets/calculating_concurrent_customer_calls-main/utils/updateActiveCalls.js
+++
@@ -1,9 +0,0 @@
-function updateActiveCalls(activeCalls, newCall) {
-  activeCalls = activeCalls.filter(call => call.endTimestamp > newCall.startTimestamp);
-
-  activeCalls.push(newCall);
-
-  return activeCalls;
-}
-
-module.exports = { updateActiveCalls };
