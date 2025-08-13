28fc1121387bf23f474da4c1b9f8598846182c77
Jesulayomy <jesulayomi081@gmail.com>
2025-08-13 11:02:49 -0400

    [ADS]: duplicates

---
+++ neet_code/arrays&hashing/containsDuplicate.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        number_set = set()
+        for num in nums:
+            if num in number_set:
+                return True
+            else:
+                number_set.add(num)
+        return False
